3212f265059d4f04b0dda75e6edec3abd1a25424
Ines Melero <imelero@vectoritcgroup.com>
2021-06-09 12:39:00 +0200

    Ejercicio iva 2

--- iva.js
+++ iva.js
@@ -5,5 +5,6 @@ function calcularPago(importe,iva){
     return importe +iva
 }
 let importe = 1000;
+let articulo = "Bicicleta";
 let iva = calcularIva(importe);
-console.log(calcularPago(importe,iva));
+console.log("Articulo: "+ articulo+ " -> A pagar "+calcularPago(importe,iva));
